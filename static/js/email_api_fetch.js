ca29e2707ad95979ed86e4c232104e7a30d575dd
lobstr <djspaul99@gmail.com>
2024-05-15 10:24:37 +0530

    fix: multiple mail breach data

--- static/js/email_api_fetch.js
+++ static/js/email_api_fetch.js
@@ -1,11 +1,17 @@
-function dataFormater (response) {
+function dataFormater(response) {
     let data = "";
-    // data += "<img src=\"" + response.LogoPath + "\" alt=\""+ response.Name +"\" style=\"width:10px;height:10px;\" >";
-    data += "<li> Name: " + response.Name + " </li>";
-    data += "<li> Domain Name: " + response.Domain + " </li>";
-    data += "<li> Breached On: " + response.BreachDate + " </li>";
-    data += "<li> Affected Emails: " + response.DataClasses[0] + " </li>";
-    data += "<li> Affected Phone Numbers: " + response.DataClasses[1] + " </li>";
+    response.data.forEach(breach => {
+        data += "<div>";
+        data += "<img src=\"" + breach.LogoPath + "\" alt=\"" + breach.Name + "\" style=\"width:50px;height:50px;\">";
+        data += "<ul>";
+        data += "<li> Name: " + breach.Name + " </li>";
+        data += "<li> Domain Name: " + breach.Domain + " </li>";
+        data += "<li> Breached On: " + breach.BreachDate + " </li>";
+        data += "<li> Description: " + breach.Description + " </li>";
+        data += "<li> Data Classes: " + breach.DataClasses.join(", ") + " </li>";
+        data += "</ul>";
+        data += "</div>";
+    });
     return data;
 }
 
@@ -16,30 +22,29 @@ const submitBtn = document.querySelector("#submit");
 
 const fetchData = async (event) => {
     event.preventDefault(); // Prevents default form submission behavior
-    if(event.key === "Enter"){
+    if (event.key === "Enter") {
         submitBtn.click();
     }
     try {
         const emailInput = await document.querySelector("#email").value;
-        if(emailInput !== "" && emailInput.search("@") !== -1) 
-            {
-                const data = { "email": emailInput };
-                let response = await fetch(apiUrl, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(data)
-                    });
-                console.log(response.status);
-                let responseData = await response.json();
-                console.log(responseData);
-                // Update DOM with response data
-                jsonDataDisplay.innerHTML = dataFormater(responseData);
-                // jsonDataDisplay.innerHTML = JSON.stringify(responseData);
-                //submitBtn.disabled = true; // Disable the submit button
-                // document.querySelector("#email").disabled = true;
-            }
+        if (emailInput !== "" && emailInput.search("@") !== -1) {
+            const data = { "email": emailInput };
+            let response = await fetch(apiUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+            console.log(response.status);
+            let responseData = await response.json();
+            console.log(responseData);
+            // Update DOM with response data
+            jsonDataDisplay.innerHTML = dataFormater(responseData);
+            // jsonDataDisplay.innerHTML = JSON.stringify(responseData);
+            //submitBtn.disabled = true; // Disable the submit button
+            // document.querySelector("#email").disabled = true;
+        }
     } catch (error) {
         console.error('Error:', error);
     }
