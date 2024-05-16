68bcc7c5094e55fe1c8f12d7a96524d5e1dd0431
subhasishmondalwork <subhasishmobdalwork+github@gmail.com>
2024-05-16 13:56:33 +0530

    alert: Secure Massage Completed

--- static/js/messageEncode.js
+++ static/js/messageEncode.js
@@ -2,6 +2,7 @@ const toggleSwitch = document.querySelector('#switch');
 const toggleText = document.querySelectorAll('.switch-x-toggletext');
 const messageInput = document.querySelector('#messageInput');
 const submitBtn = document.querySelector('.submit-btn');
+const submitDisplay = document.querySelector('#submit');
 const apiUrlen = "http://127.0.0.1:5000/api/massageEncode/encrypt";
 const apiUrlden = "http://127.0.0.1:5000/api/massageEncode/decrypt";
 let DataDisplay = document.getElementById("jsonDataDisplay");
@@ -24,7 +25,7 @@ const fetchDataEN = async (event) => {
     let responseData = await response.json();
     console.log(responseData);
     const messagep = String(responseData.message);
-    DataDisplay.innerHTML = `<span> Your encrypted message is: </span> <strong id="data">` + messagep + "</strong>"+` <button id="copyButton" class="copy-btn">Copy</button>`;
+    DataDisplay.innerHTML = `<span> Your encrypted message is: </span> <strong id="data">` + messagep + "</strong>"+` <svg id="copyButton" class="copy-btn" width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3 16V4C3 2.89543 3.89543 2 5 2H15M9 22H18C19.1046 22 20 21.1046 20 20V8C20 6.89543 19.1046 6 18 6H9C7.89543 6 7 6.89543 7 8V20C7 21.1046 7.89543 22 9 22Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>`;
     document.getElementById('copyButton').addEventListener('click', function() {
       const message = document.getElementById('data').innerText;
       if (message) {
@@ -61,7 +62,7 @@ const fetchDataDN = async (event) => {
 
     if (responseData.status !== "failed") {
       const messagep = String(atob(responseData.message));
-      DataDisplay.innerHTML = "<span> Your decrypted message is: </span> <strong id='data'>" + messagep + "</strong>" + ` <button id="copyButton" class="copy-btn">Copy</button>`;
+      DataDisplay.innerHTML = "<span> Your decrypted message is: </span> <strong id='data'>" + messagep + "</strong>" + ` <svg id="copyButton" class="copy-btn" width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3 16V4C3 2.89543 3.89543 2 5 2H15M9 22H18C19.1046 22 20 21.1046 20 20V8C20 6.89543 19.1046 6 18 6H9C7.89543 6 7 6.89543 7 8V20C7 21.1046 7.89543 22 9 22Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>`;
 
       const copyButton = document.getElementById('copyButton');
       copyButton.addEventListener('click', function() {
@@ -87,6 +88,11 @@ const fetchDataDN = async (event) => {
 toggleSwitch.addEventListener('change', () => {
   toggleText.forEach(text => text.classList.toggle('active'));
   const isEncrypt = toggleSwitch.checked;
+  if (isEncrypt) {
+    submitDisplay.innerHTML="Encrypt";
+  } else {
+    submitDisplay.innerHTML="Decrypt";
+  }
   messageInput.name = isEncrypt ? 'plain' : 'encrypted';
   messageInput.placeholder = isEncrypt ? 'Your message goes here' : 'Your encrypted message goes here';
 });
@@ -95,8 +101,10 @@ const handleSubmit = async (event) => {
   event.preventDefault();
   const isEncrypt = toggleSwitch.checked;
   if (isEncrypt) {
+    submitDisplay.innerHTML="Encrypt";
     await fetchDataEN(event);
   } else {
+    submitDisplay.innerHTML="Decrypt";
     await fetchDataDN(event);
   }
 };
@@ -109,6 +117,11 @@ document.addEventListener("DOMContentLoaded", () => {
       submitBtn.click();
     }
   };
-
   document.addEventListener("keydown", enter);
+  const isEncrypt = toggleSwitch.checked;
+  if (isEncrypt) {
+    submitDisplay.innerHTML="Encrypt";
+  } else {
+    submitDisplay.innerHTML="Decrypt";
+  }
 });
