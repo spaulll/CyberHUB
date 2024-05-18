86f66a070864843f7418716510ee32d9a1c54119
lobstr <djspaul99@gmail.com>
2024-05-18 23:24:16 +0530

    feat: escapeHtml function added to prevent XSS

--- static/js/messageEncode.js
+++ static/js/messageEncode.js
@@ -67,7 +67,8 @@ const fetchDataDN = async (event) => {
 
     if (responseData.status !== "failed") {
       const messagep = String(atob(responseData.message));
-      DataDisplay.innerHTML = "<span> Your decrypted message is: </span> <strong id='data'>" + messagep + "</strong>" + ` <svg id="copyButton" class="copy-btn" width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3 16V4C3 2.89543 3.89543 2 5 2H15M9 22H18C19.1046 22 20 21.1046 20 20V8C20 6.89543 19.1046 6 18 6H9C7.89543 6 7 6.89543 7 8V20C7 21.1046 7.89543 22 9 22Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg><span class="copiedtext" id="copied">Copied!</span>`;
+      const escapedMessage = escapeHtml(messagep);
+      DataDisplay.innerHTML = "<span> Your decrypted message is: </span> <strong id='data'>" + escapedMessage + "</strong>" + ` <svg id="copyButton" class="copy-btn" width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3 16V4C3 2.89543 3.89543 2 5 2H15M9 22H18C19.1046 22 20 21.1046 20 20V8C20 6.89543 19.1046 6 18 6H9C7.89543 6 7 6.89543 7 8V20C7 21.1046 7.89543 22 9 22Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg><span class="copiedtext" id="copied">Copied!</span>`;
 
       const copyButton = document.getElementById('copyButton');
       copyButton.addEventListener('click', function() {
@@ -89,7 +90,14 @@ const fetchDataDN = async (event) => {
   }
 };
 
-
+function escapeHtml(unsafe) {
+  return unsafe
+       .replace(/&/g, "&amp;")
+       .replace(/</g, "&lt;")
+       .replace(/>/g, "&gt;")
+       .replace(/"/g, "&quot;")
+       .replace(/'/g, "&#039;");
+}
 
 toggleSwitch.addEventListener('change', () => {
   toggleText.forEach(text => text.classList.toggle('active'));
