d0d1ba47595c0daa51f60da9eab8adf0bb359054
supriyoPurkait <supriyopurkait58@gmail.com>
2024-05-19 00:31:56 +0530

    fix:api error

--- static/js/messageEncode.js
+++ static/js/messageEncode.js
@@ -15,7 +15,7 @@ const fetchDataEN = async (event) => {
   event.preventDefault();
   try {
     let message = messageInput.value;
-    let cipherText = btoa(message);
+    let cipherText = btoa(unescape(encodeURIComponent(message)));
     console.log(cipherText);
     const data = { "encodedMassage": cipherText };
     let response = await fetch(apiUrlen, {
@@ -28,7 +28,7 @@ const fetchDataEN = async (event) => {
     console.log(response.status);
     let responseData = await response.json();
     console.log(responseData);
-    const messagep = String(decodeURIComponent(escape(atob(responseData.message))));
+    const messagep = String(responseData.message);
     DataDisplay.innerHTML = `<span> Your encrypted message is: </span> <strong id="data">` + messagep + "</strong>"+` <svg id="copyButton" class="copy-btn" width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3 16V4C3 2.89543 3.89543 2 5 2H15M9 22H18C19.1046 22 20 21.1046 20 20V8C20 6.89543 19.1046 6 18 6H9C7.89543 6 7 6.89543 7 8V20C7 21.1046 7.89543 22 9 22Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg><span class="copiedtext" id="copied">Copied!</span>`;
     document.getElementById('copyButton').addEventListener('click', function() {
       const message = document.getElementById('data').innerText;
@@ -66,9 +66,8 @@ const fetchDataDN = async (event) => {
     const DataDisplay = document.getElementById('jsonDataDisplay'); // Fix reference to DataDisplay
 
     if (responseData.status !== "failed") {
-      const messagep = String(atob(responseData.message));
-    let escapedMessage = btoa(unescape(encodeURIComponent(message)));
-      DataDisplay.innerHTML = "<span> Your decrypted message is: </span> <strong id='data'>" + escapedMessage + "</strong>" + ` <svg id="copyButton" class="copy-btn" width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3 16V4C3 2.89543 3.89543 2 5 2H15M9 22H18C19.1046 22 20 21.1046 20 20V8C20 6.89543 19.1046 6 18 6H9C7.89543 6 7 6.89543 7 8V20C7 21.1046 7.89543 22 9 22Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg><span class="copiedtext" id="copied">Copied!</span>`;
+      const messagep = String(decodeURIComponent(escape(atob(responseData.message))));
+      DataDisplay.innerHTML = "<span> Your decrypted message is: </span> <strong id='data'>" + messagep + "</strong>" + ` <svg id="copyButton" class="copy-btn" width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3 16V4C3 2.89543 3.89543 2 5 2H15M9 22H18C19.1046 22 20 21.1046 20 20V8C20 6.89543 19.1046 6 18 6H9C7.89543 6 7 6.89543 7 8V20C7 21.1046 7.89543 22 9 22Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg><span class="copiedtext" id="copied">Copied!</span>`;
 
       const copyButton = document.getElementById('copyButton');
       copyButton.addEventListener('click', function() {
@@ -90,14 +89,7 @@ const fetchDataDN = async (event) => {
   }
 };
 
-function escapeHtml(unsafe) {
-  return unsafe
-       .replace(/&/g, "&amp;")
-       .replace(/</g, "&lt;")
-       .replace(/>/g, "&gt;")
-       .replace(/"/g, "&quot;")
-       .replace(/'/g, "&#039;");
-}
+
 
 toggleSwitch.addEventListener('change', () => {
   toggleText.forEach(text => text.classList.toggle('active'));
