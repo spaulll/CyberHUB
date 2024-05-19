41d53c05dfc8bee26e4c02cd6101bf95ab60980a
lobstr <djspaul99@gmail.com>
2024-05-19 22:48:00 +0530

    feat: waiting msg and not breached msg

--- static/js/email_api_fetch.js
+++ static/js/email_api_fetch.js
@@ -2,6 +2,11 @@ function dataFormater(response) {
     if(response.status == "failed"){
         return "<div>" + response.message + "</div>";
     }
+
+    else if(response.is_breached == false){
+        return `<div>No breach found for ${emailInput}</div>`;
+    }
+
     else{
         let data = "";
         response.data.forEach(breach => {
@@ -10,18 +15,19 @@ function dataFormater(response) {
             data += `<img src="${breach.LogoPath}" alt="${breach.Name}">`;
             data += `<div id="details">`;
             data += `<div><strong>Name:</strong> <div class="resultData">${breach.Name}</div></div>`;
-            data += `<div><strong>Domain Name:</strong> <div class="resultData">${breach.Domain}</div></div>`;
+            data += `<div><strong>Domain Name:</strong> <div class="resultData"><a href="https://${breach.Domain}">${breach.Domain}</a></div></div>`;
             data += `<div><strong>Breached On:</strong> <div class="resultData">${breach.BreachDate}</div></div>`;
-            data += `<div><strong>Data Classes:</strong> <div class="resultData">${breach.DataClasses.join(", ")}</div></div>`;
+            data += `<div><strong>Potential Leakage:</strong> <div class="resultData">${breach.DataClasses.join(", ")}</div></div>`;
             data += `</div>`; // close #details
             data += `</div>`; // close #detailsContainer
             data += `</div>`;
-            data +="<hr>"
+            data +="<hr>";
             });
         return data;
     }
 }
 
+const emailInput = document.querySelector("#email").value;
 
 const apiUrl = "http://127.0.0.1:5000/api/email-breach";
 
@@ -31,6 +37,7 @@ const submitBtn = document.querySelector(".submit-btn");
 const fetchData = async (event) => {
     event.preventDefault(); // Prevents default form submission behavior
     try {
+        jsonDataDisplay.innerHTML = "<div>Please wait...</div>";
         const emailInput = await document.querySelector("#email").value;
         if (emailInput !== "" && emailInput.search("@") !== -1) {
             const data = { "email": emailInput };
@@ -45,9 +52,10 @@ const fetchData = async (event) => {
             let responseData = await response.json();
             console.log(responseData);
             // Update DOM with response data
+        
             jsonDataDisplay.innerHTML = dataFormater(responseData);
             // jsonDataDisplay.innerHTML = JSON.stringify(responseData);
-            //submitBtn.disabled = true; // Disable the submit button
+            // submitBtn.disabled = true; // Disable the submit button
             // document.querySelector("#email").disabled = true;
         }
     } catch (error) {
