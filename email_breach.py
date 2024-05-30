b3a5554989b7f0d50c741f6efec209a7ec3aca5c
lobstr <djspaul99@gmail.com>
2024-05-31 01:19:37 +0530

    refactor: removed ic

--- email_breach.py
+++ email_breach.py
@@ -1,7 +1,7 @@
 import os
 import requests
 from dotenv import load_dotenv
-from icecream import ic
+
 
 class EmailBreach:
     def __init__(self):
@@ -31,29 +31,24 @@ class EmailBreach:
             querystring = {"email": email}
             try:
                 response = requests.get(self.url, headers=self.headers, params=querystring)
-                ic(response.status_code)
-                ic("here11")
-                ic(self.keys[self.current_key_index])
                 response_json = response.json()
-                ic(response_json)
                 if "message" in response_json and "You have exceeded" in response_json["message"]:
                     self.current_key_index = (self.current_key_index + 1) % len(self.keys)
                     continue
                 else:
                     return response_json
             except requests.RequestException as e:
-                ic("Request Error:", e)
                 return {}
 
     def getBreachInfo(self, email):
         data = self.isBreached(str(email))
-        ic(data)
+        if "message" in data and "You have exceeded" in data["message"]:
+            return {"status": "failed", "message": "All APIs are exhausted. Please try again later!"}
         
-        if not data or "data" not in data:
+        if data == None or "data" not in data:
             return {"status": "failed", "message": "Something went wrong!"}
 
         message = data.get("message", "")
-        ic(message)
         
         all_entries = []
         try:    
@@ -67,7 +62,6 @@ class EmailBreach:
                 }
                 all_entries.append(entry_info)
         except Exception as e:
-            ic("Error processing entries:", e)
             return {"status": "failed", "message": "Something went wrong!"}
         
         if all_entries:
@@ -76,5 +70,5 @@ class EmailBreach:
         return {"status": message, "data": "Not found in any breach record", "is_breached": False, "message": "not found in breach record"}
 
 if __name__ == '__main__':
-    result = EmailBreach().getBreachInfo("djspaul99@gmail.com")
+    result = EmailBreach().getBreachInfo("test@gmail.com")
     print(result)
